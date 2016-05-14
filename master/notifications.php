738a2833065ca2a00416776f61d4095ad9e2a5ba
Anshul Shrivastava <anshul.shrivastava123@gmail.com>
2016-05-14 23:30:40 +0100

    Removed unnecessary notifications

--- master/notifications.php
+++ master/notifications.php
@@ -14,19 +14,19 @@ $json = json_decode(file_get_contents($endpoint));
             <li class="dropdown-menu-header text-center">
                 <strong>Account</strong>
             </li>
-            <li class="m_2"><a href="#"><i class="fa fa-bell-o"></i> Updates <span class="label label-info">42</span></a></li>
-            <li class="m_2"><a href="#"><i class="fa fa-envelope-o"></i> Messages <span class="label label-success">42</span></a></li>
-            <li class="m_2"><a href="#"><i class="fa fa-tasks"></i> Tasks <span class="label label-danger">42</span></a></li>
-            <li><a href="#"><i class="fa fa-comments"></i> Comments <span class="label label-warning">42</span></a></li>
+            <li class="m_2"><a href="#"><i class="fa fa-bell-o"></i> Updates <span class="label label-info">0</span></a></li>
+            <li class="m_2"><a href="#"><i class="fa fa-envelope-o"></i> Messages <span class="label label-success">0</span></a></li>
+<!--            <li class="m_2"><a href="#"><i class="fa fa-tasks"></i> Tasks <span class="label label-danger">42</span></a></li>-->
+<!--            <li><a href="#"><i class="fa fa-comments"></i> Comments <span class="label label-warning">42</span></a></li>-->
             <li class="dropdown-menu-header text-center">
                 <strong>Settings</strong>
             </li>
             <li class="m_2"><a href="#"><i class="fa fa-user"></i> Profile</a></li>
             <li class="m_2"><a href="#"><i class="fa fa-wrench"></i> Settings</a></li>
-            <li class="m_2"><a href="#"><i class="fa fa-usd"></i> Payments <span class="label label-default">42</span></a></li>
-            <li class="m_2"><a href="#"><i class="fa fa-file"></i> Projects <span class="label label-primary">42</span></a></li>
-            <li class="divider"></li>
-            <li class="m_2"><a href="#"><i class="fa fa-shield"></i> Lock Profile</a></li>
+<!--            <li class="m_2"><a href="#"><i class="fa fa-usd"></i> Payments <span class="label label-default">42</span></a></li>-->
+<!--            <li class="m_2"><a href="#"><i class="fa fa-file"></i> Projects <span class="label label-primary">42</span></a></li>-->
+<!--            <li class="divider"></li>-->
+<!--            <li class="m_2"><a href="#"><i class="fa fa-shield"></i> Lock Profile</a></li>-->
             <li class="m_2"><a href="#" onclick="logout();"><i class="fa fa-lock"></i> Logout</a></li>
 
             <script type="text/javascript">
