8de3fa1fbc8c6817dd07e1569ddec52ed3d563f1
Anshul Shrivastava <anshul.shrivastava123@gmail.com>
2016-06-05 00:42:21 +0100

    Added social sharing buttons

--- master/footer.php
+++ master/footer.php
@@ -1,4 +1,7 @@
 <div class="copy">
 <!--            <p>Copyright &copy; 2016 All Rights Reserved | Designed and Developed by <a href="http://axlewebtech.com/" target="_blank">Axle Web Technologies</a> </p>-->
-    <p>Copyright &copy; 2016 All Rights Reserved | Designed and Developed by <a href="https://www.facebook.com/anshulshr" target="_blank">Anshul Shrivastava</a> &amp; <a href="https://www.facebook.com/AnkitSkJain" target="_blank">Ankit Jain</a> </p>
+    <p>Copyright &copy; 2016 All Rights Reserved | Designed and Developed by <a href="https://www.facebook.com/anshulshr" target="_blank">Anshul Shrivastava</a> &amp; <a href="https://www.facebook.com/AnkitSkJain" target="_blank">Ankit Jain</a> </p></br>
+    <div class="fb-like" data-href="https://www.facebook.com/onlinebirthdaymanager/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
+    <div class="fb-send" data-href="https://www.facebook.com/onlinebirthdaymanager/"></div>
+    <img src="images/whatsapp-share-button.png" style="width: 58px;margin-bottom: 12px"><a href="whatsapp://send?text=Try%20Birthday%20Manager%20Now!+http%3a%2f%2fonlinebirthdaymanager.com%2f"></a></p>
 	    </div>
--- master/top_row.php
+++ master/top_row.php
@@ -1,4 +1,12 @@
 <?php include_once ($_SERVER["DOCUMENT_ROOT"].json_decode(file_get_contents('./././env.json'))->website_relative_path."/helper.php"); ?>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
 <nav class="top1 navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
     <div class="navbar-header">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
