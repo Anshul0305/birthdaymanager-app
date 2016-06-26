3f2f585cc9442a4786db93ea33d5b4b62c506b63
Anshul Shrivastava <anshul.shrivastava123@gmail.com>
2016-06-26 02:50:45 +0100

    Added survey monkey popup

--- master/footer.php
+++ master/footer.php
@@ -4,4 +4,6 @@
     <div class="fb-like" data-href="https://www.facebook.com/onlinebirthdaymanager/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
     <div class="fb-send" data-href="https://www.facebook.com/onlinebirthdaymanager/"></div>
     <a href="whatsapp://send?text=Try%20Birthday%20Manager%20Now!+http%3a%2f%2fonlinebirthdaymanager.com%2f"><img src="images/whatsapp-share-button.png" style="width: 58px;margin-bottom: 12px"></a></p>
-	    </div>
+
+    <script>(function(t,e,n,s){var o,c,i;t.SMCX=t.SMCX||[],e.getElementById(s)||(o=e.getElementsByTagName(n),c=o[o.length-1],i=e.createElement(n),i.type="text/javascript",i.async=!0,i.id=s,i.src=["https:"===location.protocol?"https://":"http://","widget.surveymonkey.com/collect/website/js/zEQ2_2F1kWlofNKdul2UKwvekkuRMwtg4pPrqzdqQzD0PsRYYstwBanf8kMiD9dfnA.js"].join(""),c.parentNode.insertBefore(i,c))})(window,document,"script","smcx-sdk");</script>
+</div>
